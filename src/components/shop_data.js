fdbd0ddb4fcb199698008f681b57b7bca6b50edc
SabinaSadowska <sadowwwska@gmail.com>
2020-03-13 15:26:09 +0100

    input category

--- src/components/shop_data.js
+++ src/components/shop_data.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types'
 import Card from './Card'
 
 
 
 function ShowData(props) {
 
-    
-    const [inputValue, saveInputValue] = (useState(''))
+    const [inputValue, saveInputValue] = (useState(''));
+    const [clicked, setClick] = (useState(false));
+   
 
     const handleChange = (event) => {
         saveInputValue(event.target.value);
         event.preventDefault();
+    }
+
+    const handleCategory = (event) => {
+        
     }
 
    const renderCard = () => 
@@ -27,10 +33,20 @@ function ShowData(props) {
         <div>
             <form>
                 <input placeholder="write title" onChange={handleChange} type="text" value={inputValue}/>
+                <br></br>
+                <select>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                </select>
             </form>
             {renderCard()}
         </div>
     )
+}
+
+ShowData.propTypes = {
+    data: PropTypes.array
 }
 
 export default ShowData
